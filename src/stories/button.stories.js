97486129cbcc638838c24ea7ec3a3abdd71e2e36
Summi Ju <wntjsal19@gmail.com>
2019-10-04 22:38:53 +0900

    ejs 문법 테스트

--- src/stories/button.stories.js
+++ src/stories/button.stories.js
@@ -1,5 +1,48 @@
 import { storiesOf } from '@storybook/html'
-import buttonTemplate from '../components/button.ejs'
+import listTemplate from '../components/list.ejs'
+import ifTemplate from '../components/if.ejs'
+import functionTemplate from '../components/functions.ejs'
+import classnameTemplate from '../components/classname.ejs'
+import includeTemplate from '../components/header.ejs'
 
-storiesOf('Button', module)
-  .add('with text', () => buttonTemplate({ label: 'Button!' }))
+storiesOf('Ejs', module)
+	.add('list', () => listTemplate({ 
+		"pets": [
+			{
+				name: "Hazel"
+			}
+			, {
+				name: "Crystal"
+			}
+			, {
+				name: "Catcher"
+			}
+		]
+	}))
+
+	.add('if1', () => ifTemplate({
+		type: "A"
+	}))
+
+	.add('if2', () => ifTemplate({
+		type: "B"
+	}))
+
+	.add ('functions', () => functionTemplate ({
+			users: [
+				{name: 'Tobi', age: 2, species: 'ferret'},
+				{name: 'Loki', age: 2, species: 'ferret'},
+				{name: 'Jane', age: 6, species: 'ferret'}
+			]
+	}))
+
+	.add('classname', () => classnameTemplate({
+		selected : true,
+		currentMenu: 'dashboard' 
+	}))
+
+	.add('include', () => includeTemplate({
+
+	}))
+
+
